5a724c6fdadf8ee89568ffc4c2dc0c9ad59d6f04
Mesquini <mesquini@live.com>
2019-05-02 21:34:59 -0300

    Adicionar arquivos de projeto.

---
+++ BootSorteio/Main.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace BootSorteio
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btGerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.chkBEuquero = new System.Windows.Forms.CheckBox();
+            this.chkbVouganhar = new System.Windows.Forms.CheckBox();
+            this.chkbJaganhei = new System.Windows.Forms.CheckBox();
+            this.chkbEstados = new System.Windows.Forms.CheckBox();
+            this.chkbPaises = new System.Windows.Forms.CheckBox();
+            this.chkbCidades = new System.Windows.Forms.CheckBox();
+            this.chbEscolhe = new System.Windows.Forms.CheckBox();
+            this.txtbEscolhe = new System.Windows.Forms.TextBox();
+            this.chbMarcar = new System.Windows.Forms.CheckBox();
+            this.txtbMarcar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btGerar
+            // 
+            this.btGerar.Location = new System.Drawing.Point(206, 306);
+            this.btGerar.Name = "btGerar";
+            this.btGerar.Size = new System.Drawing.Size(75, 23);
+            this.btGerar.TabIndex = 0;
+            this.btGerar.Text = "Gerar";
+            this.btGerar.UseVisualStyleBackColor = true;
+            this.btGerar.Click += new System.EventHandler(this.btGerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.AcceptsTab = true;
+            this.txtEmail.Location = new System.Drawing.Point(70, 22);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(211, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.AcceptsTab = true;
+            this.txtUrl.Location = new System.Drawing.Point(70, 102);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(211, 20);
+            this.txtUrl.TabIndex = 6;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.AcceptsTab = true;
+            this.txtSenha.Location = new System.Drawing.Point(70, 62);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(211, 20);
+            this.txtSenha.TabIndex = 5;
+            // 
+            // chkBEuquero
+            // 
+            this.chkBEuquero.AutoSize = true;
+            this.chkBEuquero.Location = new System.Drawing.Point(20, 163);
+            this.chkBEuquero.Name = "chkBEuquero";
+            this.chkBEuquero.Size = new System.Drawing.Size(72, 17);
+            this.chkBEuquero.TabIndex = 7;
+            this.chkBEuquero.Text = "Eu quero!";
+            this.chkBEuquero.UseVisualStyleBackColor = true;
+            // 
+            // chkbVouganhar
+            // 
+            this.chkbVouganhar.AutoSize = true;
+            this.chkbVouganhar.Location = new System.Drawing.Point(106, 163);
+            this.chkbVouganhar.Name = "chkbVouganhar";
+            this.chkbVouganhar.Size = new System.Drawing.Size(84, 17);
+            this.chkbVouganhar.TabIndex = 8;
+            this.chkbVouganhar.Text = "Vou ganhar!";
+            this.chkbVouganhar.UseVisualStyleBackColor = true;
+            // 
+            // chkbJaganhei
+            // 
+            this.chkbJaganhei.AutoSize = true;
+            this.chkbJaganhei.Location = new System.Drawing.Point(206, 163);
+            this.chkbJaganhei.Name = "chkbJaganhei";
+            this.chkbJaganhei.Size = new System.Drawing.Size(75, 17);
+            this.chkbJaganhei.TabIndex = 9;
+            this.chkbJaganhei.Text = "Já ganhei!";
+            this.chkbJaganhei.UseVisualStyleBackColor = true;
+            // 
+            // chkbEstados
+            // 
+            this.chkbEstados.AutoSize = true;
+            this.chkbEstados.Location = new System.Drawing.Point(106, 186);
+            this.chkbEstados.Name = "chkbEstados";
+            this.chkbEstados.Size = new System.Drawing.Size(64, 17);
+            this.chkbEstados.TabIndex = 10;
+            this.chkbEstados.Text = "Estados";
+            this.chkbEstados.UseVisualStyleBackColor = true;
+            // 
+            // chkbPaises
+            // 
+            this.chkbPaises.AutoSize = true;
+            this.chkbPaises.Location = new System.Drawing.Point(206, 186);
+            this.chkbPaises.Name = "chkbPaises";
+            this.chkbPaises.Size = new System.Drawing.Size(57, 17);
+            this.chkbPaises.TabIndex = 11;
+            this.chkbPaises.Text = "Paises";
+            this.chkbPaises.UseVisualStyleBackColor = true;
+            // 
+            // chkbCidades
+            // 
+            this.chkbCidades.AutoSize = true;
+            this.chkbCidades.Location = new System.Drawing.Point(20, 186);
+            this.chkbCidades.Name = "chkbCidades";
+            this.chkbCidades.Size = new System.Drawing.Size(64, 17);
+            this.chkbCidades.TabIndex = 12;
+            this.chkbCidades.Text = "Cidades";
+            this.chkbCidades.UseVisualStyleBackColor = true;
+            // 
+            // chbEscolhe
+            // 
+            this.chbEscolhe.AutoSize = true;
+            this.chbEscolhe.Location = new System.Drawing.Point(20, 140);
+            this.chbEscolhe.Name = "chbEscolhe";
+            this.chbEscolhe.Size = new System.Drawing.Size(102, 17);
+            this.chbEscolhe.TabIndex = 13;
+            this.chbEscolhe.Text = "Escolher a frase";
+            this.chbEscolhe.UseVisualStyleBackColor = true;
+            this.chbEscolhe.CheckedChanged += new System.EventHandler(this.valida);
+            // 
+            // txtbEscolhe
+            // 
+            this.txtbEscolhe.AcceptsTab = true;
+            this.txtbEscolhe.Location = new System.Drawing.Point(39, 163);
+            this.txtbEscolhe.Name = "txtbEscolhe";
+            this.txtbEscolhe.Size = new System.Drawing.Size(211, 20);
+            this.txtbEscolhe.TabIndex = 14;
+            this.txtbEscolhe.Visible = false;
+            // 
+            // chbMarcar
+            // 
+            this.chbMarcar.AutoSize = true;
+            this.chbMarcar.Location = new System.Drawing.Point(128, 140);
+            this.chbMarcar.Name = "chbMarcar";
+            this.chbMarcar.Size = new System.Drawing.Size(95, 17);
+            this.chbMarcar.TabIndex = 15;
+            this.chbMarcar.Text = "Marcar amigos";
+            this.chbMarcar.UseVisualStyleBackColor = true;
+            this.chbMarcar.CheckedChanged += new System.EventHandler(this.valida);
+            // 
+            // txtbMarcar
+            // 
+            this.txtbMarcar.AcceptsTab = true;
+            this.txtbMarcar.Location = new System.Drawing.Point(229, 140);
+            this.txtbMarcar.Name = "txtbMarcar";
+            this.txtbMarcar.Size = new System.Drawing.Size(52, 20);
+            this.txtbMarcar.TabIndex = 16;
+            this.txtbMarcar.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 355);
+            this.Controls.Add(this.txtbMarcar);
+            this.Controls.Add(this.chbMarcar);
+            this.Controls.Add(this.txtbEscolhe);
+            this.Controls.Add(this.chbEscolhe);
+            this.Controls.Add(this.chkbCidades);
+            this.Controls.Add(this.chkbPaises);
+            this.Controls.Add(this.chkbEstados);
+            this.Controls.Add(this.chkbJaganhei);
+            this.Controls.Add(this.chkbVouganhar);
+            this.Controls.Add(this.chkBEuquero);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btGerar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Boot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btGerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.CheckBox chkBEuquero;
+        private System.Windows.Forms.CheckBox chkbVouganhar;
+        private System.Windows.Forms.CheckBox chkbJaganhei;
+        private System.Windows.Forms.CheckBox chkbEstados;
+        private System.Windows.Forms.CheckBox chkbPaises;
+        private System.Windows.Forms.CheckBox chkbCidades;
+        private System.Windows.Forms.CheckBox chbEscolhe;
+        private System.Windows.Forms.TextBox txtbEscolhe;
+        private System.Windows.Forms.CheckBox chbMarcar;
+        private System.Windows.Forms.TextBox txtbMarcar;
+    }
+}
+
---
+++ BootSorteio/Main.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.IO;
+using System.Linq;
+using Cookie = OpenQA.Selenium.Cookie;
+
+namespace BootSorteio
+{
+    public partial class Main : Form
+    {
+        IWebDriver driver; 
+        private ToolTip meuToolTip;
+
+        int repete = 5;
+        string url;
+        string email;
+        string senha;
+        public Main()
+        {
+            InitializeComponent();
+
+            meuToolTip = new ToolTip();
+            txtSenha.PasswordChar = '*';
+            houver();
+        }
+
+        private void btGerar_Click(object sender, EventArgs e)
+        {
+            
+
+            if (valida() == true)
+            {
+                driver = new ChromeDriver();
+                login(email, senha, url);
+                Thread.Sleep(1000);
+                checkBox_verifica();
+            }
+            
+        }
+        private void valida(object sender, EventArgs e)
+        {
+            if (chbEscolhe.Checked)
+            {
+                chkbVouganhar.Visible = false;
+                chkbJaganhei.Visible = false;
+                chkBEuquero.Visible = false;
+                chkbCidades.Visible = false;
+                chkbEstados.Visible = false;
+                chkbPaises.Visible = false;
+                chbMarcar.Visible = false;
+
+                txtbEscolhe.Visible = true;
+
+            }
+            else
+            {
+                chkbVouganhar.Visible = true;
+                chkbJaganhei.Visible = true;
+                chkBEuquero.Visible = true;
+                chkbCidades.Visible = true;
+                chkbEstados.Visible = true;
+                chkbPaises.Visible = true;
+                chbMarcar.Visible = true;
+                txtbEscolhe.Visible = false;
+            }
+
+             if (chbMarcar.Checked)
+            {
+                txtbMarcar.Visible = true;
+            }else
+                txtbMarcar.Visible = false;
+        }
+        public void checkBox_verifica()
+        {
+            Thread.Sleep(1000);
+            if (chbEscolhe.Checked)
+            {
+
+                Thread.Sleep(1000);
+                repetir(txtbEscolhe.Text.Trim());
+            }
+            else if (chbMarcar.Checked)
+            {
+                Thread.Sleep(1000);
+                marcar(txtbMarcar.Text.Trim());
+            }
+
+            else if (chkbVouganhar.Checked)
+            {
+                Thread.Sleep(1000);
+                repetir("Vou ganhar!");
+            }
+            else if(chkbJaganhei.Checked)
+            {
+                Thread.Sleep(1000);
+                repetir("Já ganhei!");
+            }
+            else if(chkBEuquero.Checked)
+            {
+                Thread.Sleep(1000);
+                repetir("Eu quero!");
+            }
+            else if(chkbCidades.Checked)
+            {
+                Thread.Sleep(1000);
+                cidade();
+            }
+            else if(chkbEstados.Checked)
+            {
+                Thread.Sleep(1000);
+                estado();
+            }
+            else if(chkbPaises.Checked)
+            {
+                Thread.Sleep(1000);
+                pais();
+            }
+        }
+
+        public void login(string email, string senha, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+
+            var _cookies = driver.Manage().Cookies.AllCookies;
+
+            foreach (Cookie cookie in _cookies)
+            {
+                driver.Manage().Cookies.AddCookie(cookie);
+            }
+
+            driver.FindElement(By.ClassName("tdiEy")).Click();
+            Thread.Sleep(2000);
+
+            var login = driver.FindElement(By.Name("username"));
+            login.SendKeys(email);
+            login = driver.FindElement(By.Name("password"));
+            login.SendKeys(senha);
+            login.Submit();
+        }
+
+        public bool valida()
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text.Trim()))
+            {
+                MessageBox.Show("Email é obrigatorio","Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmail.Focus();
+                return false;
+                
+            }
+            if (string.IsNullOrEmpty(txtSenha.Text.Trim()))
+            {
+                MessageBox.Show("Senha é obrigatorio", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtSenha.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtUrl.Text.Trim()))
+            {
+                MessageBox.Show("URL é obrigatorio", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUrl.Focus();
+                return false;
+            }
+
+            email = txtEmail.Text.Trim();
+            senha = txtSenha.Text;
+            url = txtUrl.Text;
+
+            return true;
+        }
+        public void repetir(string texto)
+        {
+            for (int i = 0; 100 >= i; i++)
+            {
+                for (int j = 0; repete >= j; j++)
+                {
+                    //da um tempo para a pagina carregar
+                    Thread.Sleep(1000);
+                    //procurar um textarea e clica para dps escrever
+                    driver.FindElement(By.TagName("textarea")).Click();
+                    Thread.Sleep(1000);
+                    driver.FindElement(By.TagName("textarea")).SendKeys(texto);
+                    Thread.Sleep(1000);
+                    //procurar o botao com o tipo de enviar
+                    driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+                    Thread.Sleep(1000);
+                    driver.Navigate().GoToUrl(url);
+                    Thread.Sleep(62000);
+                }
+
+                Thread.Sleep(62000);//1,2min
+            }
+
+            driver.Quit();
+        }
+        public void cidade()
+        {
+            //Declaro o StreamReader para o caminho onde se encontra o arquivo 
+            StreamReader rd = new StreamReader(@"cidades.csv");
+            //Declaro uma string que será utilizada para receber a linha completa do arquivo 
+            string linha = null;
+            //Declaro um array do tipo string que será utilizado para adicionar o conteudo da linha separado 
+            string[] linhaseparada = null;
+            //realizo o while para ler o conteudo da linha 
+            while ((linha = rd.ReadLine()) != null)
+            {
+                //com o split adiciono a string 'quebrada' dentro do array 
+                linhaseparada = linha.Split(';');
+                //aqui incluo o método necessário para continuar o trabalho 
+                for(int i = 0; linhaseparada.Length >=i; i++)
+                {
+                    // da um tempo para a pagina carregar
+                    Thread.Sleep(1000);
+                    //procurar um textarea e clica para dps escrever
+                    driver.FindElement(By.TagName("textarea")).Click();
+                    Thread.Sleep(1000);
+                    driver.FindElement(By.TagName("textarea")).SendKeys(linhaseparada[i]);
+
+                    Thread.Sleep(1000);
+                    //procurar o botao com o tipo de enviar
+                    driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+                    Thread.Sleep(1000);
+                    driver.Navigate().GoToUrl(url);
+
+                    Thread.Sleep(62000);
+                }
+                
+            }
+
+            driver.Quit();
+        }
+
+        public void estado()
+        {
+            //Declaro o StreamReader para o caminho onde se encontra o arquivo 
+            StreamReader rd = new StreamReader(@"estados.csv");
+            //Declaro uma string que será utilizada para receber a linha completa do arquivo 
+            string linha = null;
+            //Declaro um array do tipo string que será utilizado para adicionar o conteudo da linha separado 
+            string[] linhaseparada = null;
+            //realizo o while para ler o conteudo da linha 
+            while ((linha = rd.ReadLine()) != null)
+            {
+                //com o split adiciono a string 'quebrada' dentro do array 
+                linhaseparada = linha.Split(';');
+                //aqui incluo o método necessário para continuar o trabalho 
+                for (int i = 0; linhaseparada.Length >= i; i++)
+                {
+                    // da um tempo para a pagina carregar
+                    Thread.Sleep(1000);
+                    //procurar um textarea e clica para dps escrever
+                    driver.FindElement(By.TagName("textarea")).Click();
+                    Thread.Sleep(1000);
+                    driver.FindElement(By.TagName("textarea")).SendKeys(linhaseparada[i]);
+
+                    Thread.Sleep(1000);
+                    //procurar o botao com o tipo de enviar
+                    driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+                    Thread.Sleep(1000);
+                    driver.Navigate().GoToUrl(url);
+
+                    Thread.Sleep(62000);
+                }
+
+            }
+
+            driver.Quit();
+        }
+        public void pais()
+        {
+            //Declaro o StreamReader para o caminho onde se encontra o arquivo 
+            StreamReader rd = new StreamReader(@"paises.csv");
+            //Declaro uma string que será utilizada para receber a linha completa do arquivo 
+            string linha = null;
+            //Declaro um array do tipo string que será utilizado para adicionar o conteudo da linha separado 
+            string[] linhaseparada = null;
+            //realizo o while para ler o conteudo da linha 
+            while ((linha = rd.ReadLine()) != null)
+            {
+                //com o split adiciono a string 'quebrada' dentro do array 
+                linhaseparada = linha.Split(';');
+                //aqui incluo o método necessário para continuar o trabalho 
+                for (int i = 0; linhaseparada.Length >= i; i++)
+                {
+                    // da um tempo para a pagina carregar
+                    Thread.Sleep(1000);
+                    //procurar um textarea e clica para dps escrever
+                    driver.FindElement(By.TagName("textarea")).Click();
+                    Thread.Sleep(1000);
+                    driver.FindElement(By.TagName("textarea")).SendKeys(linhaseparada[i]);
+
+                    Thread.Sleep(1000);
+                    //procurar o botao com o tipo de enviar
+                    driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+                    Thread.Sleep(1000);
+                    driver.Navigate().GoToUrl(url);
+
+                    Thread.Sleep(62000);
+                }
+
+            }
+
+            driver.Quit();
+        }
+        public void marcar(string qtd)
+        {
+            StreamReader rd = new StreamReader(@"amigos.csv");
+            //Declaro uma string que será utilizada para receber a linha completa do arquivo 
+            string linha = null;
+            int cont = 0;
+            //Declaro um array do tipo string que será utilizado para adicionar o conteudo da linha separado 
+            string[] linhaseparada = null;
+            //realizo o while para ler o conteudo da linha 
+            while ((linha = rd.ReadLine()) != null)
+            {
+                //com o split adiciono a string 'quebrada' dentro do array 
+                linhaseparada = linha.Split(';');
+                //aqui incluo o método necessário para continuar o trabalho 
+                for (int i = 0; linhaseparada.Length >= i; i++)
+                {
+                    cont = 0;
+                    // da um tempo para a pagina carregar
+                    Thread.Sleep(1000);
+                    //procurar um textarea e clica para dps escrever
+                    driver.FindElement(By.TagName("textarea")).Click();
+                    Thread.Sleep(1000);
+
+                    while (cont != int.Parse(qtd))
+                    {
+                        driver.FindElement(By.TagName("textarea")).SendKeys("@"+linhaseparada[i]);
+                        driver.FindElement(By.TagName("textarea")).SendKeys(", ");
+                        i++; cont++;
+                    }
+
+                    Thread.Sleep(1000);
+                    //procurar o botao com o tipo de enviar
+                    driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+                    Thread.Sleep(1000);
+                    driver.Navigate().GoToUrl(url);
+
+                    Thread.Sleep(62000);
+                }
+
+            }
+
+            driver.Quit();
+        }
+        private void houver()
+        {
+            meuToolTip.SetToolTip(chkbCidades, "5564 Cidades");
+            meuToolTip.SetToolTip(chkbEstados, "26 Estados");
+            meuToolTip.SetToolTip(chkbPaises, "241 Paises");
+
+            
+        }
+
+        
+    }
+}
